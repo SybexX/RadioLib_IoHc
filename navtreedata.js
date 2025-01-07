524524975584f626dddf847fd413f26896ce3827
SybexX <Heinrich-Tuning@web.de>
2025-01-07 23:21:59 +0000

    Deploying to gh-pages from  @ 2ca2ddf968215da2366a41d866cf74b8090f75f9 🚀

--- navtreedata.js
+++ navtreedata.js
@@ -27,7 +27,7 @@ var NAVTREE =
   [ "RadioLib", "index.html", [
     [ "RadioLib Documentation", "index.html", null ],
     [ "Todo List", "todo.html", null ],
-    [ "Modules", "modules.html", "modules" ],
+    [ "Topics", "topics.html", "topics" ],
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
@@ -55,13 +55,12 @@ var NAVTREE =
 var NAVTREEINDEX =
 [
 "_a_f_s_k_8h_source.html",
-"class_l_r1120.html",
-"class_physical_layer.html#a7f8d12a90158864a8ebc471b0ec89dc9",
-"class_radio_lib_print.html#a2e6857e72af3ebca1607ccd5d8201c6c",
-"class_s_x1278.html#ac0be7586b8e40355bbd29d78ae9941d1",
-"class_si443x.html#ae188dec3175a3aec96844a613e911500",
-"group__status__codes.html#gac4185b9eaead4de110763759f01e1f4f",
-"struct_s_s_t_v_mode__t.html#ae3d67bbc9815c38bea17ec070c8c0096"
+"class_l_r1120.html#ac34c2ec79e4a36cf0423acb308538811",
+"class_physical_layer.html#acb45b2342400a03a914a91d2ac7fb488",
+"class_s_x1261.html",
+"class_s_x127x.html#a75cb0d8810a5ce025d3bf5b51ca263d6",
+"classn_r_f24.html#af79757dbbd62e080c1bad2e608ab940c",
+"lr1120__transceiver__0101_8h.html#aa6490d72623bacfc624619b3c378ac5d"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
--- search/classes_b.js
+++ search/classes_b.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['pagerclient_753',['PagerClient',['../class_pager_client.html',1,'']]],
-  ['physicallayer_754',['PhysicalLayer',['../class_physical_layer.html',1,'']]],
-  ['picohal_755',['PicoHal',['../class_pico_hal.html',1,'']]],
-  ['pihal_756',['PiHal',['../class_pi_hal.html',1,'']]]
+  ['pagerclient_0',['PagerClient',['../class_pager_client.html',1,'']]],
+  ['physicallayer_1',['PhysicalLayer',['../class_physical_layer.html',1,'']]],
+  ['picohal_2',['PicoHal',['../class_pico_hal.html',1,'']]],
+  ['pihal_3',['PiHal',['../class_pi_hal.html',1,'']]]
 ];
